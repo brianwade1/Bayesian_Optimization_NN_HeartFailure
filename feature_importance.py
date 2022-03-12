08039c33c250f075c1f8838d04b3b3980d78aa5f
b-wade <brianwade221@yahoo.com>
2022-03-11 17:38:15 -0800

    additonal checking & cleaning. return cleaned.csv

--- feature_importance.py
+++ feature_importance.py
@@ -2,15 +2,13 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
-import seaborn as sns
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.linear_model import LogisticRegression
 
-# display all of the columns of the dataframe in the notebook
-pd.pandas.set_option('display.max_columns',None)
-
-%matplotlib inline
+# Set Constants
+RANDOM_STATE = 42
 
 # Read Data
 data_path = os.path.join('Data', 'heart.csv')
@@ -25,3 +23,7 @@ data.head()
 x_train, x_val_test, y_train, y_val_test = train_test_split(data, target, test_size = 0.2, random_state = RANDOM_STATE)
 x_val, x_test, y_val, y_test = train_test_split(x_val_test, y_val_test, test_size = 0.5, random_state = RANDOM_STATE)
 
+# Logistic Regression
+logReg_model = LogisticRegression(random_state = RANDOM_STATE)
+logReg_model.fit(x_train, y_train)
+coef_values = logReg_model.coef_[0]
